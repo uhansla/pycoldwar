137cd9ea5a51d6034a7ec3488b8c00ab765fadd1
uhansla <umesh.hansla@gmail.com>
2025-04-28 12:52:36 +0400

    extracted helo and ground loadouts

---
+++ loadouts/ground_assault.py
@@ -0,0 +1,27 @@
+import dcs.vehicles as vehicles
+
+ground_templates = [
+    [
+        'M-113',
+        'M1043 HMMWV Armament',
+        'M1043 HMMWV Armament',
+        'M-113',
+    ],
+    [
+        'M-113',
+        'M-60',
+        'M-60',
+        'M-113',
+    ],
+    [
+        'BRDM-2',
+        'BRDM-2',
+        'BMP-1',
+    ],
+    [
+        'BMP-1',
+        'T-55',
+        'T-55',
+        'BMP-1',
+    ],
+]
